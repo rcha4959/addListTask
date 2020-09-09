180ffb1775115abc31d22bdcaf7af9c540d07612
Rakesh Chandra <rakesh.ragamalika@gmail.com>
2020-09-09 17:32:45 +1000

    updated files with codes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.querySelector("#enter");
 var input = document.querySelector("#userinput");
 var ul = document.querySelector("ul")
-var li = document.querySelectorAll("li");
+
 
 
 button.addEventListener("click", clickItems);
@@ -12,7 +12,7 @@ button.addEventListener("click", clickItems);
 function clickItems() {
 	if (input.value.length > 0){
 	var li = document.createElement("li")
-	li.appendChild(document.createTextNode("input".value));
+	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value="";
 	}
@@ -20,13 +20,4 @@ function clickItems() {
 }
 
 
-function addRadio() {
-	var radio = document.createElement("input.type")
-	input.type="radio";
-	li.appendChild(radio);
-}
-
-
-function myfunction() {
-  $("#delete").remove()
-}
+ 
